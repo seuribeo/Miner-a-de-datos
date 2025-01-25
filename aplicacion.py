225934121edfbc277172dfec05312e02aa4d5830
seuribeo <seuribeo@unal.edu.co>
2025-01-25 11:47:45 -0500

    Create aplicacion.py

---
+++ aplicacion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+  st.title("Clasificación de la base de datos mnist")
+  st.markdown("Subir imagen para ser clasificada")
+
+if __name__=="__main__":
+  main()
