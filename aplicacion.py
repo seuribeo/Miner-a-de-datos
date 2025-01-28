d87ab275667398375911361e50e4201ceefe325f
seuribeo <seuribeo@unal.edu.co>
2025-01-28 18:32:48 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert("L")
+  image = image.resize((28, 28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
@@ -11,7 +19,13 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption = "imagen subida")
 
-    
+    preprocessed_image = preprocessed_image(image)
+
+    st.image(preprocessed_image, caption = "imagen subida")
+
+
+
+
 
 if __name__ == "__main__":
   main()
