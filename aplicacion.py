0df6bb0e6bb891df77c22be16919c14a6e6fa279
seuribeo <seuribeo@unal.edu.co>
2025-01-28 18:21:06 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -4,5 +4,8 @@ def main():
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Subir imagen para ser clasificada")
 
+uploaded_file = st.file_uploaded("Selecciona una imagen (PNG, JPG, JPEG...)", type = ["jpg", "png", "jpeg"])
+
+
 if __name__=="__main__":
   main()
