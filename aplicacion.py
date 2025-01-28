f1bb2d406acc3b8e23b09fa52b981c9f8325a458
seuribeo <seuribeo@unal.edu.co>
2025-01-28 18:59:49 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -6,20 +6,18 @@ import gzip
 import pickle
 import sklearn
 
-
 def preprocess_image(image):
-  image = image.convert("L")
-  image = image.resize((28, 28))
+  image = image.convert('L') # convertir a escala de grises
+  image = image.resize((28,28))
   image_array = img_to_array(image) / 255.0
   image_array = np.expand_dims(image_array, axis=0)
   return image_array
 
 def load_model():
   filename = "modelo_entrenado.pkl.gz"
-  with gzip.open(filename, "rb") as f:
+  with gzip.open(filename, 'rb') as f:
     model = pickle.load(f)
   return model
-
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
@@ -31,19 +29,16 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption = "imagen subida")
 
-    preprocessed_image = preprocess_image(image)
-
+    preprocessed_image = preprocess_image(image) # (1,28,28)
+   
     st.image(preprocessed_image, caption = "imagen subida")
 
     if st.button("Clasificar imagen"):
       st.markdown("Imagen clasificada")
       model = load_model()
 
-      prediction = model.predict(preprocessed_image.reshape(1,-1))
+      prediction = model.predict(preprocessed_image.reshape(1,-1)) # (1, 784)
       st.markdown(f"La imagen fue clasificada como: {prediction}")
-
-
-    
 
 
 if __name__ == "__main__":
