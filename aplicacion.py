39dc8ace7fb9c78ea1e1edc8ef183a5fb5745b10
seuribeo <seuribeo@unal.edu.co>
2025-01-28 18:26:12 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,10 +1,17 @@
 import streamlit as st
+from PIL import Image
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
   uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
+
+  if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption = "imagen subida")
+
+    
 
 if __name__ == "__main__":
   main()
