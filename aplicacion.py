c84f8f2daf707c56df9dc37f6edcd47f6464d660
seuribeo <seuribeo@unal.edu.co>
2025-01-28 18:22:42 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 def main():
-  st.title("Clasificación de la base de datos mnist")
-  st.markdown("Subir imagen para ser clasificada")
+  st.title("Clasificación de la base de datos MNIST")
+  st.markdown("Sube una imagen para clasificar")
 
-uploaded_file = st.file_uploaded("Selecciona una imagen (PNG, JPG, JPEG...)", type = ["jpg", "png", "jpeg"])
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
   main()
